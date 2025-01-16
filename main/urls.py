6b44379402c69ce90932bae28c9fbb595c1f715b
All Code Joker <allcodejoker@gmail.com>
2025-01-16 16:24:40 +0100

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('register_user/', views.register_user, name='register_user'),
+    path('login_user/', views.login_user, name='login_user'),
+    path('logout_user/', views.logout_user, name='logout_user'),
 ]
