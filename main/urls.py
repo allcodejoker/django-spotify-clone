d8e0ac13cba707500a256351c55614e578f14bd1
All Code Joker <allcodejoker@gmail.com>
2025-01-23 18:35:57 +0100

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('register_user/', views.register_user, name='register_user'),
     path('login_user/', views.login_user, name='login_user'),
     path('logout_user/', views.logout_user, name='logout_user'),
+    path('create_playlist/', views.create_playlist, name='create_playlist'),
+    path('view_playlist/<int:pk>/', views.view_playlist, name='view_playlist'),
+    path('all_playlists/', views.all_playlists, name='all_playlists'),
 ]
